933ffaaa078f0c9e8da32d77d84851400a93eef2
Connect <Connect@joye.ai>
2021-07-31 20:52:50 +0530

    Test MP4

--- src/containers/DeepBreath/index.tsx
+++ src/containers/DeepBreath/index.tsx
@@ -111,7 +111,7 @@ export class DeepBreath extends React.PureComponent<IDeepBreathProps, IDeepBreat
                   </div>
                 </div>
                 <div className="player-wrapper">
-                  <ReactPlayer className="react-player" light playsinline muted={!this.state.audioMute} playing={this.state.isPlaying} pip={false} stopOnUnmount={this.state.isStop} url={[{ src: saysomethingVideo, type: "video/mp4" }]} width="100%" height="100%" />
+                  <ReactPlayer controls className="react-player" light playsinline muted={!this.state.audioMute} playing={this.state.isPlaying} pip={false} stopOnUnmount={this.state.isStop} url={[{ src: saysomethingVideo, type: "video/mp4" }]} width="100%" height="100%" />
                   <div className="checkbox" style={{ marginTop: "10px" }}>
                     <input type="checkbox" defaultChecked={this.state.audioMute} onChange={this.handleChangeChk} />
                     <label className="checkbox-text">Turn audio on</label>
