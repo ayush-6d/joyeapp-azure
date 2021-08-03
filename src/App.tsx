e9cfdb3c32d4be977e5210d1950e30dc5d030810
Dharmesh Kashyap <dharmeshkashyap143@gmail.com>
2021-08-03 14:18:16 +0530

    sso test

--- src/App.tsx
+++ src/App.tsx
@@ -14,38 +14,6 @@ export default class App extends React.Component<IAppProps, IAppState> {
     msTeams.initialize();
     msTeams.registerOnThemeChangeHandler(this.updateTheme.bind(this));
     msTeams.getContext(context => { this.updateTheme(context.theme) });
-  //   msTeams.authentication.getAuthToken({
-  //               successCallback: (result) => {
-  //                   alert(result)
-  //               },
-  //               failureCallback: function (error) {
-  //                   alert(error)
-  //     }
-  // });
-  function getClientSideToken() {
-
-        return new Promise((resolve, reject) => {
-          alert("getClientSideToken");
-            msTeams.authentication.getAuthToken({
-                successCallback: (result) => {
-                    alert(JSON.stringify(result));
-                    resolve(result);
-                },
-                failureCallback: function (error) {
-                    reject("Error getting token: " + error);
-                    alert(JSON.stringify(error));
-                }
-            });
-
-        });
-
-    }
-     getClientSideToken()
-        .then((clientSideToken) => {
-           alert(JSON.stringify(clientSideToken));
-        }).catch((error) => {
-           alert(JSON.stringify(error));
-        })
   }
   
   render() {
