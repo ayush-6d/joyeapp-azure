3e5fe1dafa9c0ef92441ef710cc7a3e05f3281c9
Joyeconnect <87625288+Joyeconnect@users.noreply.github.com>
2021-09-02 10:22:53 +0530

    draft
    draft

--- src/containers/Dashboards/Categories/Main/page.tsx
+++ src/containers/Dashboards/Categories/Main/page.tsx
@@ -78,6 +78,7 @@ export default class Page extends React.PureComponent<IPage, IPageState> {
     }
 
     processPrediction() {
+        console.log('processPrediction');
         if (this.prediction.data.success) this.props.history.push("/pre-pie-chart");
         else if (this.prediction.data.gibberish) {
             this.setState({ isMic: false, recordingState: 'init', pageState: 'record' });
@@ -103,6 +104,7 @@ export default class Page extends React.PureComponent<IPage, IPageState> {
                 if (translateSpeechToTextResult.pid !== this.processId) return;
                 let predictionResult: any = await speechService.predictionEx(this.processId, translateSpeechToTextResult.data);
                 if (predictionResult.pid !== this.processId) return;
+                this.prediction = predictionResult.data;
                 if (this.process) this.processPrediction();
             } else if (this.state.recordingState === "confirm") {
                 if (this.prediction === null) this.process = true;
