bf146a2d354331ac8e64bc040574ff47c57f3207
Aditya Gupta <aditya.gupta@hiteshi.com>
2021-10-19 17:27:06 +0530

    fix of unused imports

--- src/containers/AuthEnd/index.tsx
+++ src/containers/AuthEnd/index.tsx
@@ -1,15 +1,4 @@
 import * as React from 'react';
-import './login.scss';
-import { withRouter } from 'react-router';
-import { connect } from 'react-redux';
-import { UserModel } from 'src/Models/UserModel';
-import { Logo, Brand, BasePage, PageImage } from 'components/index';
-import "./login.scss";
-const privacy = require("../../resources/icons/Privacy_icon.png");
-import { Button } from 'src/components/FormComponents/Button';
-import AuthHelper from 'src/helpers/auth-helper';
-import { firebaseInit } from 'src/services/firebase';
-import Popup from 'src/components/Popup';
 
 
 export const AuthEndComp = () => {
