b55fa71e29abf08395956cb140a22f99f2041f7c
Aditya Gupta <aditya.gupta@hiteshi.com>
2021-10-19 18:30:10 +0530

    changes for access token

--- src/containers/AuthEnd/index.tsx
+++ src/containers/AuthEnd/index.tsx
@@ -39,24 +39,34 @@ export const AuthEndComp = () => {
         msTeams.authentication.notifyFailure(hashParams["error"]);
     } else if (hashParams["access_token"]) {
         // Get the stored state parameter and compare with incoming state
-        let expectedState = localStorage.getItem("auth.state");
-        if (expectedState !== hashParams["state"]) {
-            // State does not match, report error
-            localStorage.setItem("auth.error", JSON.stringify(hashParams));
-            msTeams.authentication.notifyFailure("StateDoesNotMatch");
-        } else {
-            // Success -- return token information to the parent page.
-            // Use localStorage to avoid passing the token via notifySuccess; instead we send the item key.
-            let key = "auth.result";
-            // TODO: not sure why this isn't being set
-            localStorage.setItem(key, JSON.stringify({
-                idToken: hashParams["id_token"],
-                accessToken: hashParams["access_token"],
-                tokenType: hashParams["token_type"],
-                expiresIn: hashParams["expires_in"]
-            }));
-            msTeams.authentication.notifySuccess(key);
-        }
+        // let expectedState = localStorage.getItem("auth.state");
+        // if (expectedState !== hashParams["state"]) {
+        //     // State does not match, report error
+        //     localStorage.setItem("auth.error", JSON.stringify(hashParams));
+        //     msTeams.authentication.notifyFailure("StateDoesNotMatch");
+        // } else {
+        //     // Success -- return token information to the parent page.
+        //     // Use localStorage to avoid passing the token via notifySuccess; instead we send the item key.
+        //     let key = "auth.result";
+        //     // TODO: not sure why this isn't being set
+        //     localStorage.setItem(key, JSON.stringify({
+        //         idToken: hashParams["id_token"],
+        //         accessToken: hashParams["access_token"],
+        //         tokenType: hashParams["token_type"],
+        //         expiresIn: hashParams["expires_in"]
+        //     }));
+        //     msTeams.authentication.notifySuccess(key);
+        // }
+        let key = "auth.result";
+        // TODO: not sure why this isn't being set
+        localStorage.setItem('accessToken', hashParams["access_token"])
+        localStorage.setItem(key, JSON.stringify({
+            idToken: hashParams["id_token"],
+            accessToken: hashParams["access_token"],
+            tokenType: hashParams["token_type"],
+            expiresIn: hashParams["expires_in"]
+        }));
+        msTeams.authentication.notifySuccess(key);
     } else {
         // Unexpected condition: hash does not contain error or access_token parameter
         localStorage.setItem("auth.error", JSON.stringify(hashParams));
